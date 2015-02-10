6919719018ef410ae74a253c9d4dfd387f3063ed
lincanbin <lincanbin@hotmail.com>
2015-02-11 00:35:41 +0800

    Format code and add end tag in DEMO.

--- example.php
+++ example.php
@@ -1,17 +1,17 @@
 <?php
 $UploadAvatarMessage = '';
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-	if($_FILES['Avatar']['size'] && $_FILES['Avatar']['size'] < 1048576){
-		require(dirname(__FILE__)."/src/ImageResize.class.php");
-		$UploadAvatar = new ImageResize('Avatar');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if ($_FILES['Avatar']['size'] && $_FILES['Avatar']['size'] < 1048576) {
+		require(dirname(__FILE__) . "/src/ImageResize.class.php");
+		$UploadAvatar  = new ImageResize('Avatar');
 		$LUploadResult = $UploadAvatar->Resize(256, 'large.png', 100);
 		$MUploadResult = $UploadAvatar->Resize(48, 'middle.png', 90);
 		$SUploadResult = $UploadAvatar->Resize(24, 'small.png', 90);
-		if($LUploadResult && $MUploadResult && $SUploadResult)
+		if ($LUploadResult && $MUploadResult && $SUploadResult)
 			$UploadAvatarMessage = 'Success';
 		else
 			$UploadAvatarMessage = 'Failure. This file may not be image.';
-	}else{
+	} else {
 		$UploadAvatarMessage = 'File is too large! ';
 	}
 }
@@ -21,7 +21,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 		Make sure that the path can be written.
 	</p>
 	<p>
-		<?php echo $UploadAvatarMessage; ?>
+		<?php
+echo $UploadAvatarMessage;
+?>
 	</p>
 	<p>
 		<input type="file" name="Avatar">
@@ -29,3 +31,4 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	<p>
 		<input type="submit" value="Upload and Resize" name="submit" />
 	</p>
+</form>
